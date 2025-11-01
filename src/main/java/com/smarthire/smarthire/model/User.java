5a62246080c54f915da7ceeab2cf7058b78edbca
31UTKARSH07 <3107utkarshpathak@gmail.com>
2025-11-01 20:07:33 +0530

    Initial project setup

---
+++ src/main/java/com/smarthire/smarthire/model/User.java
@@ -0,0 +1,26 @@
+package com.smarthire.smarthire.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String fullName;
+    private String email;
+    private String password;
+    private String role;
+    private String skills;
+    private String bio;
+    private String resumeUrl;
+}
---
+++ src/main/java/com/smarthire/smarthire/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.smarthire.smarthire.repository;
+
+import com.smarthire.smarthire.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
+
+
+
