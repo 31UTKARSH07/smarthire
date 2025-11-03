8340c8901559edb361e77141215768c07dd95b53
31UTKARSH07 <3107utkarshpathak@gmail.com>
2025-11-04 00:00:08 +0530

    created CustomUserDetails,customUserDetailsService and SignupRequest

--- src/main/java/com/smarthire/smarthire/dto/SignupRequest.java
+++ src/main/java/com/smarthire/smarthire/dto/SignupRequest.java
@@ -1,12 +1,24 @@
 package com.smarthire.smarthire.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class SignupRequest {
+    @NotNull
+    @NotEmpty
     private String firstName;
+    @NotNull
+    @NotEmpty
     private String lastName;
+    @NotNull
+    @NotEmpty
     private String email;
+    @NotNull
+    @NotEmpty
     private String password;
-    private String role;
+    @NotNull
+    @NotEmpty
+    private String role;//student or recruiter
 }
---
+++ src/main/java/com/smarthire/smarthire/security/CustomUserDetailsService.java
@@ -0,0 +1,20 @@
+package com.smarthire.smarthire.security;
+
+import com.smarthire.smarthire.repository.UserRepository;
+import org.springframework.security.core.userdetails.*;
+import com.smarthire.smarthire.model.User;
+
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository repo;
+    public CustomUserDetailsService(UserRepository repo){
+        this.repo = repo;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = repo.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: "+email));
+        return new CustomUserDetails(user);
+    }
+
+
+}
